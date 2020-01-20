19f2e4e1f0767cbf1f0fd3c8a25bf36c97fcd503
AG6GR <slhe@alumni.princeton.edu>
2020-01-20 15:55:43 -0800

    Add V4L to setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Basic update
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade -y
+
+# V4L
+sudo apt-get install -y v4l-utils
 
 # Python stuff
-sudo apt-get install python3-pip python3-scipy python3-numpy python3-matplotlib
+sudo apt-get install -y python3-pip python3-scipy python3-numpy python3-matplotlib
 
 # Install VNC
-sudo apt-get install tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
+sudo apt-get install -y tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
 
 # Setup VNC password
 vncserver
