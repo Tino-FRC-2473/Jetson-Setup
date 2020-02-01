8dd410ad56376ddb717222e46672cd33c21ca309
Sunny He <AG6GR@users.noreply.github.com>
2020-02-01 12:41:12 -0800

    Add some more basic tools

--- setup.sh
+++ setup.sh
@@ -3,8 +3,11 @@
 # Basic update
 sudo apt-get update && sudo apt-get upgrade -y
 
-# V4L
-sudo apt-get install -y v4l-utils
+# Basic tools
+sudo apt-get install -y screen tmux
+
+# V4L and Video tools
+sudo apt-get install -y v4l-utils guvcview
 
 # Python stuff
 sudo apt-get install -y python3-pip python3-scipy python3-numpy python3-matplotlib
